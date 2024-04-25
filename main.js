940949577e377a8c1a239f06a639de639ba16a08
SayfiddinMirkamolov <sayfiddinmirkamolov7@gmail.com>
2024-04-25 06:53:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+// boolean 1
+
+//let a=+prompt("a  soni  kiriting")
+//console.log(a%2==0);
+ 
+//boolean 2 
+
+//let a =+prompt("a sonini kiriting ")
+//let b =+prompt(" b  sonini kiriting")
+
+//console.log(a>=0 || b<-2)
+
+
+// boolean  3
+
+//let a =+prompt("a sonini kiriting ")
+//let b =+prompt(" b  sonini kiriting")
+//let c =+prompt("c sonini kiriting")
+
+//console.log(a<b && b<c)
+
+//boolean 4
+
+//let a =+prompt("a sonini kiriting ")
+//let b =+prompt(" b  sonini kiriting")
+
+//console.log(a%2==1 && b%2==1)
+
+// boolean 5
+
+// let a =+prompt("a sonini kiriting ")
+// let b =+prompt(" b  sonini kiriting")
+
+// console.log(a%2==1 || b%2==1)
+
+// boolean 6
+
+// let a =+prompt("a sonini kiriting ")
+// let b =+prompt(" b  sonini kiriting")
+
+// console.log((a%2==1 && b%2==0) || (a%2==0 && b%2==1))
+
+//boolean 7 
+
+// let a =+prompt("a sonini kiriting ")
+// let b =+prompt(" b  sonini kiriting")
+// let c =+prompt("c sonini kiriting")
+
+// console.log(a>0 && b>0 && c>0);
+
+// boolean 8 
+
+// let a =+prompt("a sonini kiriting ")
+// let b =+prompt(" b  sonini kiriting")
+// let c =+prompt("c sonini kiriting")
+
+// console.log((a>0 && b<0 && c<0)||(a<0 && b<0 && c>0)||(a<0 && b>0 && c<0));
+
+// boolean 9 
+
+// let a =+prompt("a sonini kiriting ")
+// let b =+prompt(" b  sonini kiriting")
+// let c =+prompt("c sonini kiriting")
+
+// console.log((a>0 && b>0 && c<0)||(a<0 && b>0 && c>0)||(a>0 && b<0 && c>0));
+
+// boolean 10
+
+// let a = +prompt("2 xonali  son kiriting ")
+
+// console.log("kiritgan soningiz 2 xonali juft son bulsa true  chiqadi :"+(a>9&& a<100 && a%2==0));
+
+// boolean 11 
+
+// let a = +prompt("3 xonali  son kiriting ")
+
+// console.log("kiritgan soningiz 3 xonali toq son bulsa true  chiqadi : "+(a>99&& a<1000 && a%2==1));
+
+
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+1. **Number**: JavaScriptda `Number` turi o'zgaruvchilarni sonlarga aylantirish uchun ishlatiladi. Misol uchun, `Number("123")` kodini ishga tushirganda, bu 123 sonini qaytaradi.
+
+2. **Type Conversion va Coercion**: JavaScriptda tur o'zgartirish (type conversion) qiymatni bir turdan boshqa turga o'zgartirish jarayonidir. Bu jarayon yashirin (ko'rsatilmagan, avtomatik) yoki ochiq (ko'rsatilgan, dasturchi tomonidan amalga oshirilgan) bo'lishi mumkin. Yashirin tur o'zgartirishga "coercion" deyiladi.
+
+3. **Comparison Operators**: JavaScriptda solishtirish operatorlari o'zgaruvchilar yoki qiymatlar orasidagi tenglik yoki farqlilikni aniqlash uchun ishlatiladi. Ushbu operatorlar quyidagilardir: `==`, `===`, `!=`, `!==`, `>`, `<`, `>=`, `<=`.
+
+4. **Boolean**: JavaScriptda `Boolean` turi faqat ikkita qiymatni qabul qiladi: `true` yoki `false`. Har bir qiymatning ichki boolean "truthiness" yoki "falsiness" deyiladi, ya'ni ular boolean kontekstda tekshirilganda `true` yoki `false` qiymatiga aylanadi.
+
+5. **Logical Operators**: JavaScriptda mantiqiy operatorlar o'zgaruvchilar yoki qiymatlar orasidagi mantiqiy aloqani aniqlash uchun ishlatiladi. Ushbu operatorlar quyidagilardir: `&&` (va), `||` (yoki), `!` (emas).
+
+6. **Truthy va Falsy**: JavaScriptda "truthy" va "falsy" qiymatlar boolean kontekstda tekshirilganda `true` yoki `false` qiymatiga aylanadigan qiymatlardir. Barcha qiymatlar "truthy" hisoblanadi, agar ular "falsy" sifatida belgilanmagan bo'lsa. "Falsy" qiymatlarga quyidagilar kiradi: `false`, `0`, `-0`, `0n`, `""`, `null`, `undefined`, `NaN`, va `document.all`.
+
